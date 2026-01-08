d72cf6164b1d11cdd356ac86ff9fb421d453670d
Emirhan Çam <55931785+emirhancam@users.noreply.github.com>
2026-01-08 14:59:53 +0300

    Create 739.cpp

---
+++ stack/739.Daily Temperatures/739.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures)
+    {
+        int n = temperatures.Size();
+        vector<int> result(n, 0); //Başlangıçta tüm elemanları 0 ile başlattık.
+        stack<int> st; // Indexleri tutacak olan stack.
+
+        
+    }
+};
