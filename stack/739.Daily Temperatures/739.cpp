bdd022000cdfa6b5307bff210964dd649df0681f
Emirhan Çam <55931785+emirhancam@users.noreply.github.com>
2026-01-08 15:15:24 +0300

    Update 739.cpp

--- stack/739.Daily Temperatures/739.cpp
+++ stack/739.Daily Temperatures/739.cpp
@@ -6,6 +6,16 @@ public:
         vector<int> result(n, 0); //Başlangıçta tüm elemanları 0 ile başlattık.
         stack<int> st; // Indexleri tutacak olan stack.
 
-        
+        for (int i=0; i<n; i++)
+        {
+            while (!st.empty() && temperatures[st.top()] < temperatures[i])
+            {
+                int prevIndex = st.top();
+                st.pop();
+                result[prevIndex] = i - prevIndex;
+            }
+            st.push(i);
+        }
+        return result;
     }
 };
